502553f1fe531f00790c4bfb21985c2b530cace0
LucasLeoHGM <120404983+LucasLeoHGM@users.noreply.github.com>
2025-11-07 16:51:36 -0300

    Atvdd

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 /**** Escreva o código abaixo 👇******/
+function somar(a, b) {
+  return a + b;
+}
 
+function multiplicar(a, b) {
+  return a * b;
+}
 
+function subtrair(a, b) {
+  return a - b;
+}
 
+function dividir(a, b) {
+  if (b === 0) {
+    throw new Error("Erro: divisão por zero");
+  }
+  return a / b;
+}
 
+function calcularMedia(...numeros) {
+  if (numeros.length === 0) {
+    throw new Error("Forneça ao menos um número!");
+  }
+  const soma = numeros.reduce((total, n) => total + n, 0);
+  return soma / numeros.length;
+}
 
+function elevarPotencia(base, expoente) {
+  return Math.pow(base, expoente);
+}
 
+function calcularAreaRetangulo(largura, altura) {
+  return largura * altura;
+}
 
+function verificarParOuImpar(numero) {
+  return numero % 2 === 0 ? "par" : "ímpar";
+}
+
+function calcularRaizQuadrada(numero) {
+  if (numero < 0) {
+    throw new Error("Número negativo não possui raiz real!");
+  }
+  return Math.sqrt(numero);
+}
+
+function calcularHipotenusa(cateto1, cateto2) {
+  return Math.sqrt(cateto1 ** 2 + cateto2 ** 2);
+}
 
 /**** Escreva o código acima ☝️******/
 
